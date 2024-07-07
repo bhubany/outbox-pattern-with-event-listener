231d343e2c064f13872677950ec57e03308443c6
bhubany <yadav.bhuban.by@gmail.com>
2024-07-07 23:58:29 +0545

    feat(): add shell script to start postgres service

--- start-postgres.sh
+++ start-postgres.sh
@@ -5,23 +5,67 @@ NETWORK_NAME="outbox-net"
 VOLUME_NAME="pgdata"
 COMPOSE_FILE="docker-compose.yaml"
 DOCKERFILE="postgres.dockerfile"
-# CONTAINER_NAME="outbox_event_listner_pg"
-CONTAINER_NAME="outboxwitheventlistner_postgres_1"
+CONTAINER_NAME="outbox_event_listner_pg"
 
+#create network
+create_network() {
+    echo "Creating network..."
+    if [ $(docker network ls | grep -w "$NETWORK_NAME" | wc -l) -eq 0 ]; then
+        docker network create "$NETWORK_NAME"
+    else
+        echo "Network $NETWORK_NAME already exists"
+    fi
+}
+
+#crerate volume
+create_volume() {
+    echo "Creating volume..."
+    if [ $(docker volume ls | grep -w "$VOLUME_NAME" | wc -l) -eq 0 ]; then
+        docker volume create "$VOLUME_NAME"
+    else
+        echo "Volume $VOLUME_NAME already exists"
+    fi
+}
 
 # create service method
 create_service(){
-    echo "Creating service"
+    create_network
+    create_volume
+    echo "Running docker-compose ..."
+    docker-compose up -d
 }
 
 
 #check if container is already running
-if 
-    docker ps -a --format '{{.Names}}' | grep -q "^$CONTAINER_NAME$" ; 
-then 
-    echo "Service is already running with container name: $CONTAINER_NAME"
-    echo "Do you want to remove existing and recreate it ?\n(Y/N):"
-    read res
+if docker ps --format '{{.Names}}' | grep -q "^$CONTAINER_NAME$"; then
+    echo "Postgres service is already running with container: $CONTAINER_NAME"
+elif docker ps -a --format '{{.Names}}' | grep -q "^$CONTAINER_NAME$"; then 
+    echo "Postgres service already exists with container name: $CONTAINER_NAME but is stopped"
+    echo "Please select an option from below:"
+    echo "1. Start the service"
+    echo "2. Remove the existing container and recreate "
+    echo "3. Exit"
+    read -p "Enter your choice: " choice
+
+    case $choice in
+        1)
+            echo "Starting the service"
+            docker start "$CONTAINER_NAME"
+            ;;
+        2)
+            echo "Removing the existing service"
+            docker rm -f "$CONTAINER_NAME"
+            echo "Creating new service"
+            create_service
+            ;;
+        3)
+            echo "Exiting"
+            exit 0
+            ;;
+        *)
+            echo "Invalid choice"
+            ;;
+    esac
+else
     create_service
-    echo $res
 fi
